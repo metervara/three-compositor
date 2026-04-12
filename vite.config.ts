8a858842f76de34e444b53e7bd9e5b73cec0f3dd
Patrik Svensson <patrik.svensson@manyone.com>
2026-04-12 15:05:20 +0200

    fix: types fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import dts from "vite-plugin-dts";
 export default defineConfig({
   plugins: [
     glsl(),
-    dts({ tsconfigPath: "./tsconfig.json" }),
+    dts({ entryRoot: "src" }),
   ],
   resolve: {
     alias: {
